f67bdefb3ece953436bf8aa52d74c60b1182260f
JordanSmith314 <44780363+JordanSmith314@users.noreply.github.com>
2019-10-14 11:28:21 -0700

    adding code

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 
-sensors.touch1.onEvent(ButtonEvent.Pressed, function () {
+brick.buttonDown.onEvent(ButtonEvent.Pressed, function () {
+    brick.setStatusLight(StatusLight.GreenFlash)
 
+    brick.showString("Hello world", 1)
+    brick.showString("everything is awsome", 2)
 })
